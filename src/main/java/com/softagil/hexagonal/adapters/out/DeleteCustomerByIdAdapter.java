1b1f54b99332f3897827fc925c7b074b6b3eadd2
Cléber Marcelo Alves de Barros <cleberbarros.ti@gmail.com>
2023-04-13 09:52:43 -0300

    Criando adapter para delete customer

---
+++ src/main/java/com/softagil/hexagonal/adapters/out/DeleteCustomerByIdAdapter.java
@@ -0,0 +1,18 @@
+package com.softagil.hexagonal.adapters.out;
+
+import com.softagil.hexagonal.adapters.out.repository.CustomerRepository;
+import com.softagil.hexagonal.application.ports.out.DeleteCustomerByIdOutputPort;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DeleteCustomerByIdAdapter implements DeleteCustomerByIdOutputPort {
+
+    @Autowired
+    private CustomerRepository customerRepository;
+
+    @Override
+    public void delete(String id) {
+        customerRepository.deleteById(id);
+    }
+}
